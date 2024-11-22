3491b7259ae3087e8e5393a26a56da253cba215e
DanTinho <161615116+DanTinho@users.noreply.github.com>
2024-11-21 23:35:41 -0300

    Add files via upload

--- calculadoraIMC/imc.js
+++ calculadoraIMC/imc.js
@@ -10,7 +10,7 @@ function perguntarPeso() {
     if(imc < 18.5) {
     resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, você é considerado <b>ABAIXO DO PESO</b>.";
     } else if (imc > 18.6 && imc < 24.9) {
-    resposta.innerHTML = "O valor do seu IMC é de<b>" + imc.toFixed(1) + "</b>, então, seu peso é considerado <b>NORMAL</b>.";
+    resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, seu peso é considerado <b>NORMAL</b>.";
     } else if (imc > 25 && imc < 29.9) {
     resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, você é considerado <b>ACIMA DO PESO</b>.";
     } else if (imc > 30) {
