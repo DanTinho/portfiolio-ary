ba09f0abd6895bea415f78814be782851a2eee20
DanTinho <161615116+DanTinho@users.noreply.github.com>
2024-11-21 23:33:46 -0300

    Add files via upload

--- calculadoraIMC/imc.js
+++ calculadoraIMC/imc.js
@@ -1,19 +1,20 @@
 function perguntarPeso() {
 
-    peso = prompt("Qual seu peso?");
-    altura = prompt("Qual sua altura em metros?");
-
+    peso = document.getElementById('input_peso').value;
+    altura = document.getElementById('input_altura').value;
+    
     let imc = peso / (altura * altura);
 
     let resposta = window.document.getElementById('valorImc');
 
     if(imc < 18.5) {
-    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está abaixo do peso.<p>";
+    resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, você é considerado <b>ABAIXO DO PESO</b>.";
     } else if (imc > 18.6 && imc < 24.9) {
-    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está no peso normal.<p>";
+    resposta.innerHTML = "O valor do seu IMC é de<b>" + imc.toFixed(1) + "</b>, então, seu peso é considerado <b>NORMAL</b>.";
     } else if (imc > 25 && imc < 29.9) {
-    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está acima do peso.<p>";
+    resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, você é considerado <b>ACIMA DO PESO</b>.";
     } else if (imc > 30) {
-    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está obeso.<p>";
+    resposta.innerHTML = "O valor do seu IMC é de <b>" + imc.toFixed(1) + "</b>, então, você é considerado <b>OBESO</b>.";
     }
+
 }
